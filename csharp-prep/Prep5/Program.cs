42a1fc71f8219b1641eb7b1b680a8737880b1892
collinspg <collinsokolie90@gmail.com>
2024-01-11 17:38:43 +0100

    Added a new code for functions in C#

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcomeMessage();
+
+        string userEnteredName = PromptUserForName();
+        int userNumber = PromptUserForNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userEnteredName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserForName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserForNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
