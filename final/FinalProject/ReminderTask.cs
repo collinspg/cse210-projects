a88ace8d255d32670f316fbdac23c1ac7a8fe676
collinspg <collinsokolie90@gmail.com>
2024-02-17 16:31:12 +0100

    Added ReminderTask class as a placeholder for future implementation.

---
+++ final/FinalProject/ReminderTask.cs
@@ -0,0 +1,11 @@
+// ReminderTask.cs
+using System;
+
+public class ReminderTask : Task
+{
+    public ReminderTask(string title, string description, DateTime deadline, string priority)
+        : base(title, description, deadline, priority)
+    {
+        
+    }
+}
