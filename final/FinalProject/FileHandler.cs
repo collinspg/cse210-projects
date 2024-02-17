456218bb2d54ea39a33bf46412ae5d2cd6d21e4f
collinspg <collinsokolie90@gmail.com>
2024-02-17 16:30:36 +0100

    Added skeleton for saving tasks to a file and loading from a file.

---
+++ final/FinalProject/FileHandler.cs
@@ -0,0 +1,13 @@
+public class FileHandler
+{
+    public static void SaveTasksToFile(List<Task> tasks, string filePath)
+    {
+        // Implement saving tasks to a file
+    }
+
+    public static List<Task> LoadTasksFromFile(string filePath)
+    {
+        // Implement loading tasks from a file
+        return new List<Task>();
+    }
+}
